4990ac71644e054a2488f0541e730b323bab3b2d
Rendy <rendyrn047@gmail.com>
2021-06-12 11:31:23 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Bang-Kernel By rnxyr
+kernel.string=Exation Kernel V1 By rnxyr
 do.devicecheck=1
 do.modules=0
 do.systemless=1
